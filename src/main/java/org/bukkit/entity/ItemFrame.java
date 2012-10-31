d7fa62f417e7e5cd35451a8b3ed52d3dc4d06cdb
h31ix <effectsdude@gmail.com>
2012-10-31 01:01:45 -0400

    Add API for ItemFrames and reorder common Hanging/Attachable functions
    As well as adding methods for ItemFrames, this moves several values previously contained in Painting to Hanging, as they will now be shared by both classes.

--- src/main/java/org/bukkit/entity/ItemFrame.java
+++ src/main/java/org/bukkit/entity/ItemFrame.java
@@ -1,6 +1,83 @@
 package org.bukkit.entity;
 
+import org.bukkit.inventory.ItemStack;
+
 /**
  * Represents an Item Frame
  */
-public interface ItemFrame extends Hanging {}
+public interface ItemFrame extends Hanging {
+    /**
+     * An enum to specify the rotation of the ItemFrame's item
+     */
+    public enum Rotation {
+        /**
+         * No rotation
+         */
+        NONE,
+        /**
+         * Rotated clockwise
+         */
+        CLOCKWISE,
+        /**
+         * Flipped upside-down
+         */
+        FLIPPED,
+        /**
+         * Rotated counter-clockwise
+         */
+        COUNTER_CLOCKWISE;
+
+        private static final Rotation [] rotations = values();
+
+        /**
+         * Rotate clockwise one value, as if the frame was right-clicked by a player.
+         *
+         * @return The new rotation
+         */
+        public Rotation rotateClockwise() {
+            return rotations[(this.ordinal() + 1) & 0x3];
+        }
+
+        /**
+         * Attempts to get the Rotation with the given value
+         *
+         * @param value Value of the Rotation to get
+         * @return Rotation if found, or null
+         */
+        public static Rotation getRotation(final int value) {
+            if (rotations.length > value) {
+                return rotations[value];
+            } else {
+                return null;
+            }
+        }
+    }
+
+    /**
+     * Get the item in this frame
+     *
+     * @return The item
+     */
+    public ItemStack getItem();
+
+    /**
+     * Set the item in this frame
+     *
+     * @param item The new item
+     */
+    public void setItem(ItemStack item);
+
+    /**
+     * Get the rotation of the frame's item
+     *
+     * @return The direction
+     */
+    public Rotation getRotation();
+
+    /**
+     * Set the rotation of the frame's item
+     *
+     * @param rotation The new rotation
+     */
+    public void setRotation(Rotation rotation);
+}
