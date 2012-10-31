cb945cbf0159ece6f9c55106a9191c6c9e7085cd
h31ix <effectsdude@gmail.com>
2012-10-30 23:58:59 -0400

    Revert "Add new events for Hanging entities, deprecate old Painting events."
    This reverts commit ee8e54b6594430f763223ca6ada6c1fca42a4a72.

--- src/main/java/org/bukkit/event/painting/PaintingBreakEvent.java
+++ src/main/java/org/bukkit/event/painting/PaintingBreakEvent.java
@@ -1,16 +1,12 @@
 package org.bukkit.event.painting;
 
-import org.bukkit.Warning;
 import org.bukkit.entity.Painting;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
 /**
  * Triggered when a painting is removed
- * @deprecated Use {@link org.bukkit.event.hanging.HangingBreakEvent} instead.
  */
-@Deprecated
-@Warning(reason="This event has been replaced by HangingBreakEvent")
 public class PaintingBreakEvent extends PaintingEvent implements Cancellable {
     private static final HandlerList handlers = new HandlerList();
     private boolean cancelled;
@@ -56,9 +52,7 @@ public class PaintingBreakEvent extends PaintingEvent implements Cancellable {
         OBSTRUCTION,
         /**
          * Removed by water flowing over it
-         * @deprecated Water no longer triggers hanging entities to be removed
          */
-        @Deprecated
         WATER,
         /**
          * Removed by destroying the block behind it, etc
